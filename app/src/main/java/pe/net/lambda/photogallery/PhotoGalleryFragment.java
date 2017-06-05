3cffd8f9c9567b20fc97f468e9191100ffe1cb72
Gary Briceño <gary.briceno@gmail.com>
2017-06-05 14:46:42 -0500

    Version 1: Creación del proyecto y uso de HTTP

---
+++ app/src/main/java/pe/net/lambda/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,35 @@
+package pe.net.lambda.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Gary on 5/06/2017.
+ */
+
+public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance(){
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return v;
+    }
+}
---
+++ app/src/main/java/pe/net/lambda/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,38 @@
+package pe.net.lambda.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+* Se utiliza la clase SingleFragmentActivity como un patron para que el Activity
+* pueda generar los fragment. Este requiere del layout activity_fragment
+*/
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if( fragment == null ){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
+    }
+}
